d3ce8b0ba09b0aea8d20232dbd98d7e79c6e7f1f
gangchanghwan <lgodl1598@naver.com>
2024-11-13 14:51:23 +0900

    add: chroma adding

--- src/main/java/org/service/taxchatbot/config/ChatModelConfig.java
+++ src/main/java/org/service/taxchatbot/config/ChatModelConfig.java
@@ -1,7 +1,14 @@
 package org.service.taxchatbot.config;
 
+import org.springframework.ai.chroma.ChromaApi;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.ChromaVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,7 +16,23 @@ import org.springframework.web.client.RestTemplate;
 public class ChatModelConfig {
 
     @Bean
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+    RestClient.Builder builder() {
+        return RestClient.builder().requestFactory(new SimpleClientHttpRequestFactory());
+    }
+
+    @Bean
+    EmbeddingModel embeddingModel() {
+        return new OpenAiEmbeddingModel(new OpenAiApi(System.getenv("SPRING_AI_OPENAI_API_KEY")));
+    }
+
+    @Bean
+    ChromaApi chromaApi(RestClient.Builder restClientBuilder) {
+        String chromaUrl = "http://localhost:8000";
+        return new ChromaApi(chromaUrl, restClientBuilder);
+    }
+
+    @Bean
+    ChromaVectorStore chromaVectorStore(EmbeddingModel embeddingModel, ChromaApi chromaApi) {
+        return new ChromaVectorStore(embeddingModel, chromaApi, "tax_docs", true);
     }
 }
